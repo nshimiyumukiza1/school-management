b8bbab35ade891d10284f591fa67e95320a6e2ff
nshimiyumukiza <nshimiyumukizaerneste99@gmail.com>
2025-10-22 13:45:50 +0300

    protection router

--- backend/src/routers/user.router.js
+++ backend/src/routers/user.router.js
@@ -1,9 +1,17 @@
 import express from "express";
 import { loginUser, registerUser } from "../controllers/user.controller.js";
+import { protecte } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register",registerUser);
 router.post("/login",loginUser)
+
+router.get("/me",protecte,(req,res) =>{
+    res.json({
+        message:"Wellcom to your Profifle",
+        user:req.user
+    })
+})
 
 export default router;
